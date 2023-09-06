5afcf4840b3e5e5b7f87968249196d5702a64235
ManasaSita <maanumns04@gmail.com>
2023-09-06 22:40:58 +0530

    Day2: Login page UI and validation improvements

--- Day2/public/loginPage.js
+++ Day2/public/loginPage.js
@@ -1,31 +1,85 @@
-$(document).ready(function () {
-    let username = $(".emailInput").val();
-    let password = $(".passwordInput").val();
+// $(document).ready(function () {
 
-    console.log("email & password", username, password);
+//     function isValidEmail(email) {
+//         console.log(email);
+//         var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//         return emailPattern.test(email);
+//     }
 
-    $("#loginForm").submit(function() {
-        event.preventDefault();
-        if (username != null && password != null){
-            setTimeout(function() {
-                window.location.href = "index2.html"; 
-            }, 2000);
-        }  else {
-            alert("Invalid login credentials. Please try again.");
-        }
-    });
+//     function isValidPassword(pswrd) {
+//         console.log("pasword", pswrd, pswrd.length >= 8);
+//         return pswrd.length >= 8;
+//     }
 
-    $("#loginButton").click(function() {
-        // event.preventDefault();
-        console.log($(this));
-        $(this).css("background-color", "black");
-        $(this).css("color", "white");
-    })
+//     $("#loginForm").submit(function() {
+//         event.preventDefault();
+
+//         var username = $("#emailInput").val();
+//         var password = $("#passwordInput").val();
+
+//         console.log("login event");
+
+//         $("#errorMessages").empty();
+
+//         if (!isValidEmail(username)) {
+//             $("#errorMessages").append("<p>Invalid email address.</p>");
+//             console.log("email validation", username);
+//             return;
+//         } else if (!isValidPassword(password)) {
+//             console.log("+++++++++", password);
+//             $("#errorMessages").append("<p>Password must be at least 8 characters long.</p>");
+//             console.log("password validation");
+//             return;
+//         } else {
+//             console.log("new window");
+//             window.location.href = "index2.html"; 
+//         }
+
+//     });
+
+//     // $("#loginButton").click(function() {
+//     //     // event.preventDefault();
+//     //     console.log($(this));
+//     //     $(this).css("background-color", "black");
+//     //     $(this).css("color", "white");
+//     // })
 
    
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function isValidEmail(email) {
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    }
+    
+    function isValidPassword(password) {
+        return password.length >= 8;
+    }
+
+    var  loginForm = document.getElementById("loginForm");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        var email = document.getElementById("emailInput").value;
+        var password = document.getElementById("passwordInput").value;
+
+        if (email.length == 0 || password.length == 0) {
+            alert("Empty EMAIL and PASSWORD!");
+        } else if(!isValidEmail(email)) {
+            alert("Invalid Email!");
+            return;            
+        } else if (!isValidPassword(password)) {
+            alert("Invalid Password!");
+            return;
+        } else {
+            window.location.href = "index2.html";
+            alert(`Logged in with email: ${email}`);
+        }
+    });
 });
-
-
 
 
 
